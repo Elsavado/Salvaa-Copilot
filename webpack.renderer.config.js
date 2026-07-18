9456072e626d29573b7cb8cc5138dc99acbc0b1f
Elsavado <104304873+Elsavado@users.noreply.github.com>
2026-07-18 14:43:54 +0300

    Add Webpack configuration for Electron renderer

---
+++ webpack.renderer.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  target: 'electron-renderer',
+  entry: './src/renderer/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist/renderer'),
+    filename: 'index.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/renderer/index.html',
+      filename: 'index.html',
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist/renderer'),
+    },
+    port: 3000,
+    hot: true,
+  },
+};
