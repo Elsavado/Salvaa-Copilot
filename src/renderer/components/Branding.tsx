03b3196c60c374dd36ef28fc0111680eef1f567a
Elsavado <104304873+Elsavado@users.noreply.github.com>
2026-07-18 14:12:54 +0300

    Add Branding component for product information

---
+++ src/renderer/components/Branding.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Branding: React.FC = () => {
+  return (
+    <div className="text-center py-4">
+      <p className="text-xs text-gray-500 opacity-60">
+        A Product of Salvaaa Technical Solutions
+      </p>
+    </div>
+  );
+};
+
+export default Branding;
